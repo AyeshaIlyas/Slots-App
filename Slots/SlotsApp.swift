764fc2aabf394995fb097387388c3fa8760aebd5
Ayesha Ilyas <annie16950@gmail.com>
2022-02-22 10:10:30 -0500

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Ayesha Ilyas on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
